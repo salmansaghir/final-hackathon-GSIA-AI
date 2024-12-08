fe8b25bfde61658c6e3b3610923e9eecae779ce5
salman saghir <salman.saghir1@gmail.com>
2024-12-09 02:53:27 +0500

    some chages done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+// import localFont from "next/font/local";
 import {Poppins } from 'next/font/google'
 import "./globals.css";
 
