0b7fc84fb8b4d74dc35a54baf836f19cd40a07f9
salman saghir <salman.saghir1@gmail.com>
2024-12-21 13:18:13 +0500

    some changes done

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,10 +1,6 @@
 import React from 'react'
-import { CiUser } from "react-icons/ci";
-import { CiHeart } from "react-icons/ci";
-import { CiSearch } from "react-icons/ci";
-import { IoCartOutline } from "react-icons/io5";
-import Link from 'next/link';
-import { IoIosArrowForward } from "react-icons/io";
+
+
 import Header2 from '@/components/Shop/Header2';
 import Common from '@/components/Shop/Common';
 import Filter from '@/components/Shop/Filter';
